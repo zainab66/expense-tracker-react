db8ce3c3816fb04fe05256c8deae63008258a0ac
zainab66 <zainab66r@gmail.com>
2023-05-19 11:33:19 -0400

    add ability to add transactions to app

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from './context/GlobalState';
 
 export default function AddTransaction() {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
 
   return (
     <>
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,24 +4,37 @@ import AppReducer from './AppReducer';
 // We need an initial state
 
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Book', amount: -15 },
-    { id: 2, text: 'Salary', amount: 650 },
-    { id: 3, text: 'Phone case', amount: -25 },
-    { id: 4, text: 'Video game', amount: -80 },
-  ],
+  transactions: [],
 };
 
 // Create context
 export const GlobalContext = createContext(initialState);
+
 // Provider component (provides state and actions to the components its wrapped around)
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions that make calls to reducer
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction,
+    });
+  }
 
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
       }}
     >
       {children}
